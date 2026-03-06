bccd15c926622c5aea378fe37eb9901cb76db716
Daniel Wiśniewski <daniel.wisniewski@polskipcs.pl>
2026-03-06 13:27:56 +0100

    fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-const COLORS = ['#1a0069', '#00edc2', '#ffc658', '#ff8042', '#0088fe', '#00C49F'];
+const COLORS = ['#1a0069', '#00edc2', '#ffc658', '#ff8042', '#0088fe', '#ff4444', '#2d0096'];
 
 export { COLORS };
